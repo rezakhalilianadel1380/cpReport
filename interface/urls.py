fb319391852b947f9f08f5cf2110a5c3e1253ef7
cpco <CP@WS131.cp.forest>
2025-04-23 11:18:43 +0330

    add chart to project

--- interface/urls.py
+++ interface/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path,include
-from .views import report
+from .views import report,chart_view
 urlpatterns = [
     
     path('',report),
+    path('chart',chart_view),
 ]
--- interface/views.py
+++ interface/views.py
@@ -10,3 +10,14 @@ def report(request):
 
     }
     return render(request,"report.html",context)
+
+
+
+
+def chart_view(request):
+    context = {
+        "labels": ["2024-01", "2024-02", "2024-03", "2024-04"],
+        "pressure_data": [101, 105, 110, 108],
+        "temperature_data": [23, 25, 22, 26],
+    }
+    return render(request, "chart.html", context)
